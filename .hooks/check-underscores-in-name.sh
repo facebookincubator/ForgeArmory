097e03973f1b4e3274bc708c95a655f505dd5734
Jayson Grace <jayson.e.grace@gmail.com>
2023-08-31 00:11:06 -0600

    Implemented a pre-commit hook to promote naming consistency across YAML files and directories, supplemented with a test suite for verification, and fine-tuned our pre-commit configuration for enhanced hook management.
    - Introduced a pre-commit hook to ensure consistent naming in YAML files and directories, replacing underscores with dashes.
    - Added a BATS test suite to validate the new hook's functionality.
    - Modified .pre-commit-config.yaml for improved hook configuration and exclusions.

---
+++ .hooks/check-underscores-in-name.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+for file in $(git diff --cached --name-only --diff-filter=ACM); do
+    echo "Checking file: $file"
+
+    base_name=$(basename "$file")
+    dir_name=$(dirname "$file")
+
+    # Check if the base name of the file or directory has an underscore
+    if [[ $base_name == *_* ]]; then
+
+        if [[ "$file" == *.yml ]] || [[ "$file" == *.yaml ]] || {
+                                                                  [[ ! -f "$file" ]] && [[ -d "$file" ]]
+        }; then
+
+            # Replace underscores with dashes using parameter expansion
+            new_name=${base_name//_/-}
+            # Check if the directory name is ".", which represents the root, and adjust accordingly
+            new_path=$([[ "$dir_name" == "." ]] && echo "$new_name" || echo "$dir_name/$new_name")
+
+            # Rename the file or directory and stage the change in Git
+            git mv "$file" "$new_path"
+            git reset HEAD "$file"
+
+            echo "Info: Renamed $file to $new_path"
+        fi
+    else
+        echo "$base_name does not have an underscore."
+    fi
+done
+
+exit 0
